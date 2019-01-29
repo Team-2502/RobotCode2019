45418af8535d9d6cfb162115c52b650a44b4d989
Isaac Ash-Johnson <isaacashj@gmail.com>
2019-01-28 18:22:41 -0600

    Removed a bad `requires()` statement
    Doesn't make sense to have here, should be unnecessary if this command works as intended.

--- src/main/java/com/team2502/robot2019/command/autonomous/ingredients/AbortAutoCommand.java
+++ src/main/java/com/team2502/robot2019/command/autonomous/ingredients/AbortAutoCommand.java
@@ -1,16 +1,13 @@
 package com.team2502.robot2019.command.autonomous.ingredients;
 
-import com.team2502.robot2019.Robot;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
 public class AbortAutoCommand extends InstantCommand
 {
-    public AbortAutoCommand() { requires(Robot.DRIVE_TRAIN); }
+    public AbortAutoCommand() { }
 
     @Override
-    protected void execute(){
-        Scheduler.getInstance().removeAll();
-    }
+    protected void execute() { Scheduler.getInstance().removeAll(); }
 
 }
