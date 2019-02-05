98a0b45ca2ff549a750d064e04ec81f77bc2eddd
Isaac Ash-Johnson <isaacashj@gmail.com>
2019-02-04 19:02:02 -0600

    Made same change with Cargo

--- src/main/java/com/team2502/robot2019/command/teleop/CargoActive/CargoActiveCommand.java
+++ src/main/java/com/team2502/robot2019/command/teleop/CargoActive/CargoActiveCommand.java
@@ -1,31 +1,25 @@
 package com.team2502.robot2019.command.teleop.CargoActive;
 
 import com.team2502.robot2019.Robot;
+import com.team2502.robot2019.subsystem.CargoSubsystem;
 import edu.wpi.first.wpilibj.command.Command;
 
-@Deprecated
 public class CargoActiveCommand extends Command {
 
-    private final boolean top;
+    private final CargoSubsystem.Belt belt;
     private double speed;
 
-    public CargoActiveCommand(double speed, boolean top)
+    public CargoActiveCommand(CargoSubsystem.Belt belt, double speed)
     {
-        this.top = top;
-        requires(Robot.CARGO_ACTIVE);
+//        requires(Robot.CARGO_ACTIVE);
+        this.belt = belt;
         this.speed = speed;
     }
 
     @Override
     protected void execute()
     {
-        if(top)
-        {
-            Robot.CARGO_ACTIVE.runTop(speed);
-        }
-        else {
-            Robot.CARGO_ACTIVE.runBottom(speed);
-        }
+        Robot.CARGO_ACTIVE.runIntake(belt, speed);
     }
 
     @Override
