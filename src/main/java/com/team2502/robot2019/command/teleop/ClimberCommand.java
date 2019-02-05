f0808d5c59f54e62926aceccca570eb2657adcbc
Isaac Ash-Johnson <isaacashj@gmail.com>
2019-02-04 19:01:52 -0600

    Changed climber to use enums for more precise control (specific motors)

--- src/main/java/com/team2502/robot2019/command/teleop/ClimberCommand.java
+++ src/main/java/com/team2502/robot2019/command/teleop/ClimberCommand.java
@@ -1,22 +1,26 @@
 package com.team2502.robot2019.command.teleop;
 
 import com.team2502.robot2019.Robot;
+import com.team2502.robot2019.subsystem.ClimberSubsystem;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ClimberCommand extends Command
 {
     private boolean forwards;
 
-    public ClimberCommand(boolean forwards)
+    private ClimberSubsystem.ClimberSide side;
+
+    public ClimberCommand(ClimberSubsystem.ClimberSide sides, boolean forwards)
     {
         requires(Robot.CLIMBER);
         this.forwards = forwards;
+        this.side = sides;
     }
 
     @Override
     protected void execute()
     {
-        Robot.CLIMBER.climb(forwards);
+        Robot.CLIMBER.climb(side, forwards);
     }
 
     @Override
