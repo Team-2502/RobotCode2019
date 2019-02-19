f9edc0afffbe8ca2f37288ad7bf9b30057cb95a4
Isaac Ash-Johnson <isaacashj@gmail.com>
2019-02-19 10:50:33 -0600

    I'm bad at naming things

--- src/main/java/com/team2502/robot2019/command/teleop/climber/CrawlCommand.java
+++ src/main/java/com/team2502/robot2019/command/teleop/climber/CrawlCommand.java
@@ -1,7 +1,6 @@
-package com.team2502.robot2019.command.teleop.Climber;
+package com.team2502.robot2019.command.teleop.climber;
 
 import com.team2502.robot2019.Robot;
-import com.team2502.robot2019.subsystem.ClimberSubsystem;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class CrawlCommand extends Command
