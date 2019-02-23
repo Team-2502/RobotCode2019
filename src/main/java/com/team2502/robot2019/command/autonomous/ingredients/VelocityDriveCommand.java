554d8614755dcf769862947a6050aaa5f59f36fd
Ritik Mishra <ritik.mishra314@gmail.com>
2019-02-22 22:24:04 -0600

    Remove apple, banana

--- src/main/java/com/team2502/robot2019/command/autonomous/ingredients/VelocityDriveCommand.java
+++ src/main/java/com/team2502/robot2019/command/autonomous/ingredients/VelocityDriveCommand.java
@@ -1,6 +1,5 @@
 package com.team2502.robot2019.command.autonomous.ingredients;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.team2502.robot2019.Robot;
 import edu.wpi.first.wpilibj.command.TimedCommand;
 
@@ -10,12 +9,10 @@ public class VelocityDriveCommand extends TimedCommand
     private final double rightVel;
     private final boolean brake;
 
-    private int apple = 0;
-    private int banana = 0;
     /**
- * @param leftVel
+     * @param leftVel
      * @param rightVel
- * @param time       Amount of time to run for (seconds)
+     * @param time     Amount of time to run for (seconds)
      */
     public VelocityDriveCommand(double leftVel, double rightVel, double time)
     {this(leftVel, rightVel, time, true);}
@@ -23,7 +20,7 @@ public class VelocityDriveCommand extends TimedCommand
     /**
      * @param leftVel
      * @param rightVel
-     * @param time       Amount of time to run for (seconds)
+     * @param time     Amount of time to run for (seconds)
      */
     public VelocityDriveCommand(double leftVel, double rightVel, double time, boolean brake)
     {
@@ -35,19 +32,11 @@ public class VelocityDriveCommand extends TimedCommand
         requires(Robot.DRIVE_TRAIN);
     }
 
-    @Override
-    protected void initialize()
-    {
-        apple++;
-        System.out.println("apple = " + apple);
-        banana = 0;
-    }
 
     @Override
     protected void execute()
     {
         Robot.DRIVE_TRAIN.runMotorsVelocity(leftVel, rightVel);
-        System.out.println("banana = " + banana++);
     }
 
     @Override
